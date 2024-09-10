5449efb71d91c47ad98663b0dd6c1b761fa0b8d1
SpencerChakabva <93149276+SpenceChakabva@users.noreply.github.com>
2024-09-10 19:42:26 +0200

    chore: Refactor main.go for better readability and consistency

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
-    "gorm.io/driver/sqlite"
-    "gorm.io/gorm"
-    _ "modernc.org/sqlite" // Import the modernc.org/sqlite driver
+	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	_ "modernc.org/sqlite" // Import the modernc.org/sqlite driver
 )
 
 var db *gorm.DB
@@ -16,69 +16,79 @@ var err error
 
 // Todo represents a task
 type Todo struct {
-    ID     uint   `json:"id" gorm:"primaryKey"`
-    Title  string `json:"title"`
-    Status string `json:"status"`
+	ID     uint   `json:"id" gorm:"primaryKey"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
 }
 
 func main() {
-    // Initialize the database
-    db, err = gorm.Open(sqlite.Open("file:todos.db?cache=shared&mode=rwc"), &gorm.Config{})
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Initialize the database
+	db, err = gorm.Open(sqlite.Open("file:todos.db?cache=shared&mode=rwc"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Migrate the schema
-    db.AutoMigrate(&Todo{})
+	// Migrate the schema
+	db.AutoMigrate(&Todo{})
 
-    // Initialize the router
-    router := mux.NewRouter()
+	// Initialize the router
+	router := mux.NewRouter()
 
-    // Define the routes
-    router.HandleFunc("/todos", getTodos).Methods("GET")
-    router.HandleFunc("/todos", createTodo).Methods("POST")
-    router.HandleFunc("/todos/{id}", getTodo).Methods("GET")
-    router.HandleFunc("/todos/{id}", updateTodo).Methods("PUT")
-    router.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
+	// Define the routes
+	router.HandleFunc("/todos", getTodos).Methods("GET")
+	router.HandleFunc("/todos", createTodo).Methods("POST")
+	router.HandleFunc("/todos/{id}", getTodo).Methods("GET")
+	router.HandleFunc("/todos/{id}", updateTodo).Methods("PUT")
+	router.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
 
-    // Start the server
-    log.Fatal(http.ListenAndServe(":8000", router))
+	// Start the server
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 // Handlers
 
 func getTodos(w http.ResponseWriter, r *http.Request) {
-    var todos []Todo
-    db.Find(&todos)
-    json.NewEncoder(w).Encode(todos)
+	var todos []Todo
+	db.Find(&todos)
+	json.NewEncoder(w).Encode(todos)
 }
 
 func createTodo(w http.ResponseWriter, r *http.Request) {
-    var todo Todo
-    json.NewDecoder(r.Body).Decode(&todo)
-    db.Create(&todo)
-    json.NewEncoder(w).Encode(todo)
+	var todo Todo
+	json.NewDecoder(r.Body).Decode(&todo)
+	db.Create(&todo)
+	w.WriteHeader(http.StatusCreated) // Set status code to 201
+	json.NewEncoder(w).Encode(todo)
 }
 
 func getTodo(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    var todo Todo
-    db.First(&todo, params["id"])
-    json.NewEncoder(w).Encode(todo)
+	params := mux.Vars(r)
+	var todo Todo
+	db.First(&todo, params["id"])
+	json.NewEncoder(w).Encode(todo)
 }
 
 func updateTodo(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    var todo Todo
-    db.First(&todo, params["id"])
-    json.NewDecoder(r.Body).Decode(&todo)
-    db.Save(&todo)
-    json.NewEncoder(w).Encode(todo)
+	params := mux.Vars(r)
+	var todo Todo
+	db.First(&todo, params["id"])
+	json.NewDecoder(r.Body).Decode(&todo)
+	db.Save(&todo)
+	json.NewEncoder(w).Encode(todo)
 }
 
 func deleteTodo(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)
     var todo Todo
-    db.Delete(&todo, params["id"])
-    json.NewEncoder(w).Encode("Todo deleted")
+    result := db.First(&todo, params["id"])
+    if result.Error != nil {
+        if result.Error == gorm.ErrRecordNotFound {
+            http.Error(w, "Todo not found", http.StatusNotFound)
+        } else {
+            http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+        }
+        return
+    }
+    db.Delete(&todo)
+    w.WriteHeader(http.StatusNoContent) // Set status code to 204 No Content
 }
